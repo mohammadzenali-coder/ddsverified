1b8ea5066971d1a0771ad5880c3ec05f15d277bb
mohammadzenali-coder <Mohammad.zenali@gmail.com>
2026-09-08 21:59:12 +0330

    chore: remove local CMS (cms.py, cms.bat, test_cms_rewrite.py)

--- cms.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM Local CMS launcher for ddsverified.ir
-REM Double-click this file to start the CMS in a new window.
-REM Browser opens to http://127.0.0.1:8765/ automatically.
-REM Close this window (or Ctrl+C) to stop the CMS.
-
-start "DDSVerified CMS" cmd /k "python cms.py & timeout /t 2 /nobreak >nul & start http://127.0.0.1:8765/"
